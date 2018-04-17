42b9c3f735874daa9823b66f8a7c9c323ff8039c
Daniel Work-PC <roetzer.daniel@gmail.com>
2018-04-17 08:57:06 +0200

    Added Todo Prototype

---
+++ todo/src/pages/home/home.ts
@@ -0,0 +1,60 @@
+import { DataProvider } from './../../providers/data/data';
+import { ItemDetailPage } from './../item-detail/item-detail';
+import { AddItemPage } from './../add-item/add-item';
+import { Component } from '@angular/core';
+import { NavController, ModalController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public items = [];
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public dataService: DataProvider) {
+    this.dataService.getData().then((todos) => {
+ 
+      if(todos){
+        this.items = todos;
+      }
+ 
+    });
+  }
+
+  ionViewDidLoad(){
+ 
+    /*this.items = [
+      {title: 'hi1', description: 'test1'},
+      {title: 'hi2', description: 'test2'},
+      {title: 'hi3', description: 'test3'}
+    ];*/
+ 
+  }
+ 
+  addItem(){
+    let addModal = this.modalCtrl.create(AddItemPage);
+ 
+    addModal.onDidDismiss((item) => {
+ 
+          if(item){
+            this.saveItem(item);
+          }
+ 
+    });
+ 
+    addModal.present();
+  }
+
+  saveItem(item){
+    this.items.push(item);
+    this.dataService.save(this.items);
+  }
+ 
+  viewItem(item){
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
+  }
+
+}
